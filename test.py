0977921b6f3155fc61843e880f8e52b3114ddf2b
Javier Escribano <escribano101@gmail.com>
2020-12-18 16:17:58 +0100

    Fixed typo

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ import numpy as np
 from maddpg import MADDPG
 from params import UNITY_EXE_PATH
 
-
-# Init the reacher environment and get agents, state and action info
+# Init the tennis environment and get agents, state and action info
 env, brain_name, n_agents, state_size, action_size = init_environment(UNITY_EXE_PATH, train=False)
 agent = MADDPG(state_size=state_size, action_size=action_size, n_agents=n_agents, random_seed=89)
 # Load trained weights
@@ -23,6 +22,5 @@ while True:
     dones = env_info.local_done                        
     scores += env_info.rewards                         
     states = next_states                              
-
 
 env.close()
